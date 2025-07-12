0fe9989ecad75224ca5a4e6530543b26b5865acf
Cj Gwapo <cjdagul86@gmail.com>
2025-07-12 22:23:56 +0800

    cjgwapo

--- src/Unit_Conv.java
+++ src/Unit_Conv.java
@@ -1,8 +1,27 @@
-length
-m <-> cm
+//length
+//m <-> cm
+//
+//weight
+//g <-> kg
+//
+//temperature
+// f <-> c
 
-weight
-g <-> kg
 
-temperature
- f <-> c
+
+
+class UnitConverter {
+    private final double converted_result;
+    private final double value_to_convert;
+
+    public UnitConverter(double value_to_convert, double converted_result) {
+        this.converted_result = converted_result;
+        this.value_to_convert = value_to_convert;
+    }
+
+    private static double m_to_cm(){
+
+        double converted_result =
+
+    }
+}
