813226f9f3ed45f7f2146f583c8cd760437d41b7
Cj Gwapo <cjdagul86@gmail.com>
2025-07-31 21:33:43 +0800

    cjgwapo

--- src/proj2.java
+++ src/proj2.java
@@ -1,6 +1,28 @@
 public class proj2 {
-    public static  void main(String[] args) {
 
 
+
+    public static void main(String[] args){
+        System.out.println("\nNumber Guessing Game\n");
+        String greetings = """
+                🎮 Welcome to the Number Guessing Game! 🎯
+                
+                Think you can guess the right number? Let's put your luck (and logic) to the test!
+                
+                🔢 Choose your challenge:
+                
+                Range 1 to 10 – Quick and tricky!
+                
+                Range 1 to 15 – A bit more challenging!
+                
+                Or set your own custom range – You decide how easy or hard it gets!
+                
+                🧠 You only have 5 tries, so make every guess count!
+                
+                Are you ready to play? Let the guessing begin!
+
+                """;
+        System.out.println(greetings);
+        System.out.println("1. Start\n2. Exit");
     }
 }
